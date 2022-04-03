ffff6754ebc3b464cb387d9878154560f8594c3c
iwanjo <kamanuian@gmail.com>
2022-04-03 11:29:40 +0200

    Font files

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'GT-Walsheim-Regular'
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
